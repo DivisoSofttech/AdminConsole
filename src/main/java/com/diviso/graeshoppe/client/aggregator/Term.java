adc67e95633ec908945de458dd5937120665d3b4
sruthykc <sruthy.k.c@lxisoft.com>
2020-01-29 15:12:09 +0530

    SubTerm by termid

--- src/main/java/com/diviso/graeshoppe/client/aggregator/Term.java
+++ src/main/java/com/diviso/graeshoppe/client/aggregator/Term.java
@@ -1,86 +1,84 @@
 package com.diviso.graeshoppe.client.aggregator;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 
 public class Term {
 	
 	  private Long id;
 
 	    private String title;
-	    private Set<SubTerm> subTerms = new HashSet<>();
+	    
+	    private List<SubTerm> subTerms;
 
-	    // jhipster-needle-entity-add-field - JHipster will add fields here, do not remove
-	    public Long getId() {
-	        return id;
-	    }
+		public Long getId() {
+			return id;
+		}
 
-	    public void setId(Long id) {
-	        this.id = id;
-	    }
+		public void setId(Long id) {
+			this.id = id;
+		}
 
-	    public String getTitle() {
-	        return title;
-	    }
+		public String getTitle() {
+			return title;
+		}
 
-	    public Term title(String title) {
-	        this.title = title;
-	        return this;
-	    }
+		public void setTitle(String title) {
+			this.title = title;
+		}
 
-	    public void setTitle(String title) {
-	        this.title = title;
-	    }
+		public List<SubTerm> getSubTerms() {
+			return subTerms;
+		}
 
-	    public Set<SubTerm> getSubTerms() {
-	        return subTerms;
-	    }
+		public void setSubTerms(List<SubTerm> subTerms) {
+			this.subTerms = subTerms;
+		}
 
-	    public Term subTerms(Set<SubTerm> subTerms) {
-	        this.subTerms = subTerms;
-	        return this;
-	    }
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((id == null) ? 0 : id.hashCode());
+			result = prime * result + ((subTerms == null) ? 0 : subTerms.hashCode());
+			result = prime * result + ((title == null) ? 0 : title.hashCode());
+			return result;
+		}
 
-	    public Term addSubTerm(SubTerm subTerm) {
-	        this.subTerms.add(subTerm);
-	        subTerm.setTerm(this);
-	        return this;
-	    }
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Term other = (Term) obj;
+			if (id == null) {
+				if (other.id != null)
+					return false;
+			} else if (!id.equals(other.id))
+				return false;
+			if (subTerms == null) {
+				if (other.subTerms != null)
+					return false;
+			} else if (!subTerms.equals(other.subTerms))
+				return false;
+			if (title == null) {
+				if (other.title != null)
+					return false;
+			} else if (!title.equals(other.title))
+				return false;
+			return true;
+		}
 
-	    public Term removeSubTerm(SubTerm subTerm) {
-	        this.subTerms.remove(subTerm);
-	        subTerm.setTerm(null);
-	        return this;
-	    }
+		@Override
+		public String toString() {
+			return "Term [id=" + id + ", title=" + title + ", subTerms=" + subTerms + "]";
+		}
 
-	    public void setSubTerms(Set<SubTerm> subTerms) {
-	        this.subTerms = subTerms;
-	    }
-	    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here, do not remove
-
-	    @Override
-	    public boolean equals(Object o) {
-	        if (this == o) {
-	            return true;
-	        }
-	        if (!(o instanceof Term)) {
-	            return false;
-	        }
-	        return id != null && id.equals(((Term) o).id);
-	    }
-
-	    @Override
-	    public int hashCode() {
-	        return 31;
-	    }
-
-	    @Override
-	    public String toString() {
-	        return "Term{" +
-	            "id=" + getId() +
-	            ", title='" + getTitle() + "'" +
-	            "}";
-	    }
+	    
+	    
+	    
 	}
 	 
 	  
